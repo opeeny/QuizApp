66f05e40dc7d63be40f82c0a01de7aa4307e4b06
opeeny <godfreyopeeny@gmail.com>
2020-03-20 11:07:02 +0000

    Add files

--- src/App.js
+++ src/App.js
@@ -2,25 +2,70 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+export default function AuthorQuiz({ turnData }) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container-fluid">
+      <Hero />
+      <Turn {...turnData} />
+      <Continue />
+      <Footer />
     </div>
   );
 }
+
+const Hero = () => {
+  return (
+    <div className="row">
+      <div className="jumbotran col-10 offset-1">
+        <h1>Author Quiz</h1>
+        <p>Select the book written by the author shown</p>
+      </div>
+    </div>
+  );
+};
+const Book = ({ title }) => {
+  return (
+    <div className="answer">
+      <h1>{title}</h1>
+    </div>
+  );
+};
+const Turn = ({ author, books }) => {
+  return (
+    <div
+      className="row turn"
+      style={{ backgroundColor: "greenyellow", color: "red" }}
+    >
+      <div className="col-4 offset-1">
+        <img src={author.imageUrl} className="authorImage" alt="Author" />
+      </div>
+      <div className="col-6">
+        {books.map(title => (
+          <Book title={title} key={title} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+const Continue = () => {
+  return <div className="row" />;
+};
+
+const Footer = () => {
+  return (
+    <div id="footer" className="row">
+      <div className="col-12">
+        <p className="text-muted credit">
+          You guys give a thumb{" "}
+          <a href="https://opeeny.github.io" target="blank">
+            me
+          </a>
+        </p>
+      </div>
+    </div>
+  );
+};
+
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -5,9 +5,57 @@ import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+const authors = [
+    {
+      name: "Robert Kiyosaki",
+      imageUrl:
+        "https://assets.entrepreneur.com/content/1x1/300/20160310203057-robert-standard.jpeg?width=200&crop=1:1",
+      imageSource: "Google",
+      books: ["Rich Dad", "Fake", "Cash flow game"]
+    },
+    {
+      name: "Opeeny Godfrey",
+      imageUrl: "https://opeeny.github.io/images/opeeny.jpg?width=200&crop=1:1",
+      imageSource: "opeeny.github.io",
+      books: ["FIQ", "Back in the day", "Fail forward"]
+    },
+    {
+      name: "Jin frey",
+      imageUrl: "https://invictafc.com/wp-content/uploads/2015/07/Jinh_Yu_Frey_Bio.jpg?width=200&crop=1:1",
+      imageSource: "opeeny.github.io",
+      books: ["BZQ", "Body build", "xenophobic"]
+    },
+    {
+      name: "Charles Dickens",
+      imageUrl:
+        "https://cdn.britannica.com/17/82717-050-5D9C010D/Charles-Dickens.jpg?width=200&crop=1:1",
+      imageSource: "opeeny.github.io",
+      books: ["Hamlet", "Macbeth", "Romeo & Juliet"]
+    }
+  ];
+
+  const state = {
+    turnData: getTurnData(authors)
+  };
+  function getTurnData(authors) {
+    const allBooks = authors.reduce((p, c, i) => {
+      return p.concat(c.books);
+    }, []);
+    const fourRandomBooks = shuffle(allBooks).slice(0, 4);
+    const answer = sample(fourRandomBooks);
+    return {
+      books: fourRandomBooks,
+      author: authors.find(author => author.books.some(title => title === answer))
+    };
+  }
+  const rootElement = document.getElementById("root");
+  ReactDOM.render(
+    <React.StrictMode>
+      <AuthorQuiz {...state} />
+    </React.StrictMode>,
+    rootElement
+  );
+  
+ReactDOM.render(<App />, document.getElementById('root'));
 serviceWorker.unregister();
